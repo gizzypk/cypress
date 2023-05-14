d75b4099a5d61ca03eacfd2b713ba0118f6044a6
pornvajanak <pornvajana.kauwatnakul@allianz.com>
2023-05-14 21:32:56 +0700

    fix html

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,18 +3,22 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   requestTimeout: 15000,
   chromeWebSecurity: false,
-  reporter: "mochawesome",
+  reporter: "cypress-mochawesome-reporter",
   video: false,
   
   reporterOptions: {
     reportDir: "cypress/report",
     charts: true,
     html: true,
-    reportPageTitle: "Cypress-cwa-bce-test-automation-report",
+    inlineAssets: true,
+    reportPageTitle: "Cypress-test-automation-report",
     embeddedScreenshots: true,
   },
 
   e2e: {
-    specPattern : ["cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}"]
+    specPattern : ["cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", "cypress/e2e/**/*.spec.{js,jsx,ts,tsx}"],
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    }
   }
 });
